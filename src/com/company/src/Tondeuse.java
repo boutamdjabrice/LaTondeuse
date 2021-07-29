8b1c0d32496cf2707d62ac3d0b73afe7423584e4
briceBoutamdja <brice.boutamdja@etu.u-pec.fr>
2021-07-29 18:06:17 +0200

    Ajout test tourner vers la droite Ajout test de verification si c'est null si pas d'initialisation implémentation méthode tourner vers la droite

--- src/com/company/src/Tondeuse.java
+++ src/com/company/src/Tondeuse.java
@@ -31,4 +31,21 @@ public class Tondeuse {
       break;
     }
   }
+
+  public void tournerVersLaDroite() {
+    switch (orientation){
+    case NORD:
+      orientation = Orientation.EST;
+      break;
+    case EST:
+      orientation = Orientation.SUD;
+      break;
+    case SUD:
+      orientation = Orientation.OUEST;
+      break;
+    case OUEST:
+      orientation = Orientation.NORD;
+      break;
+    }
+  }
 }
--- src/com/company/test/TondeuseTest.java
+++ src/com/company/test/TondeuseTest.java
@@ -5,22 +5,33 @@ import com.company.src.Tondeuse;
 import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TondeuseTest extends TestCase {
 
-    public void testOriendationInitialAuNord(){
-        Tondeuse tondeuse = new Tondeuse();
+    Tondeuse tondeuse = new Tondeuse();
+
+    public void testOrientationInitialAuNord(){
         tondeuse.setOrientation(Orientation.NORD);
         assertEquals(Orientation.NORD, tondeuse.getOrientation());
     }
 
+    public void testOrientationInitialNull(){
+        assertNull(tondeuse.getOrientation());
+    }
+
     public void testTournerVersLaGauche(){
-        Tondeuse tondeuse = new Tondeuse();
         tondeuse.setOrientation(Orientation.NORD);
         tondeuse.tournerVersLaGauche();
         assertEquals(Orientation.OUEST, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaDroite(){
+        tondeuse.setOrientation(Orientation.NORD);
+        tondeuse.tournerVersLaDroite();
+        assertEquals(Orientation.EST, tondeuse.getOrientation());
     }
 
 }
