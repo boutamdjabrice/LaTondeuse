c68d43e58a0e10d233f359030791f23a003cb3d5
briceBoutamdja <brice.boutamdja@etu.u-pec.fr>
2021-08-01 14:29:59 +0200

    ajout des tests manquants pour la couverture intégrale des cases

--- src/com/company/test/TondeuseTest.java
+++ src/com/company/test/TondeuseTest.java
@@ -25,28 +25,103 @@ public class TondeuseTest extends TestCase {
         assertEquals(Orientation.NORD, tondeuse.getOrientation());
     }
 
+    public void testOrientationInitialAuOuest(){
+        tondeuse.setOrientation(Orientation.OUEST);
+        assertEquals(Orientation.OUEST, tondeuse.getOrientation());
+    }
+
+    public void testOrientationInitialAuSud(){
+        tondeuse.setOrientation(Orientation.SUD);
+        assertEquals(Orientation.SUD, tondeuse.getOrientation());
+    }
+
+    public void testOrientationInitialAuEst(){
+        tondeuse.setOrientation(Orientation.EST);
+        assertEquals(Orientation.EST, tondeuse.getOrientation());
+    }
+
     public void testOrientationInitialNull(){
         assertNull(tondeuse.getOrientation());
     }
 
-    public void testTournerVersLaGauche(){
+    public void testTournerVersLaGaucheOrientationInitialNord(){
         tondeuse.setOrientation(Orientation.NORD);
         tondeuse.tournerVersLaGauche();
         assertEquals(Orientation.OUEST, tondeuse.getOrientation());
     }
 
-    public void testTournerVersLaDroite(){
+    public void testTournerVersLaGaucheOrientationInitialOuest(){
+        tondeuse.setOrientation(Orientation.OUEST);
+        tondeuse.tournerVersLaGauche();
+        assertEquals(Orientation.SUD, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaGaucheOrientationInitialSud(){
+        tondeuse.setOrientation(Orientation.SUD);
+        tondeuse.tournerVersLaGauche();
+        assertEquals(Orientation.EST, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaGaucheOrientationInitialEst(){
+        tondeuse.setOrientation(Orientation.EST);
+        tondeuse.tournerVersLaGauche();
+        assertEquals(Orientation.NORD, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaDroiteOrientationInitialNord(){
         tondeuse.setOrientation(Orientation.NORD);
         tondeuse.tournerVersLaDroite();
         assertEquals(Orientation.EST, tondeuse.getOrientation());
     }
 
-    public void testAvancer(){
+    public void testTournerVersLaDroiteOrientationInitialEst(){
+        tondeuse.setOrientation(Orientation.EST);
+        tondeuse.tournerVersLaDroite();
+        assertEquals(Orientation.SUD, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaDroiteOrientationInitialSud(){
+        tondeuse.setOrientation(Orientation.SUD);
+        tondeuse.tournerVersLaDroite();
+        assertEquals(Orientation.OUEST, tondeuse.getOrientation());
+    }
+
+    public void testTournerVersLaDroiteOrientationInitialOuest(){
+        tondeuse.setOrientation(Orientation.OUEST);
+        tondeuse.tournerVersLaDroite();
+        assertEquals(Orientation.NORD, tondeuse.getOrientation());
+    }
+
+    public void testAvancerOrientationNord(){
         tondeuse.setOrientation(Orientation.NORD);
         tondeuse.setCordX(1);
         tondeuse.setCordY(0);
         tondeuse.avancer(pelouse);
         assertEquals(Arrays.asList(1,1),tondeuse.getPosition());
+    }
+
+    public void testAvancerOrientationEst(){
+        tondeuse.setOrientation(Orientation.EST);
+        tondeuse.setCordX(1);
+        tondeuse.setCordY(1);
+        tondeuse.avancer(pelouse);
+        assertEquals(Arrays.asList(2,1),tondeuse.getPosition());
+    }
+
+    public void testAvancerOrientationSud(){
+        tondeuse.setOrientation(Orientation.SUD);
+        tondeuse.setCordX(1);
+        tondeuse.setCordY(1);
+        tondeuse.avancer(pelouse);
+        assertEquals(Arrays.asList(1,0),tondeuse.getPosition());
+    }
+
+    public void testAvancerOrientationOuest(){
+        tondeuse.setOrientation(Orientation.OUEST);
+        tondeuse.setCordX(1);
+        tondeuse.setCordY(1);
+        tondeuse.avancer(pelouse);
+        assertEquals(Arrays.asList(0,1),tondeuse.getPosition());
     }
 
     public void testAvancerSurUnBordInferieurGauche(){
@@ -57,8 +132,24 @@ public class TondeuseTest extends TestCase {
         assertEquals(Arrays.asList(0,0),tondeuse.getPosition());
     }
 
+    public void testAvancerSurUnBordInferieurBas(){
+        tondeuse.setOrientation(Orientation.SUD);
+        tondeuse.setCordX(0);
+        tondeuse.setCordY(0);
+        tondeuse.avancer(pelouse);
+        assertEquals(Arrays.asList(0,0),tondeuse.getPosition());
+    }
+
     public void testAvancerSurUnBordSuperieurDroit(){
         tondeuse.setOrientation(Orientation.EST);
+        tondeuse.setCordX(5);
+        tondeuse.setCordY(5);
+        tondeuse.avancer(pelouse);
+        assertEquals(Arrays.asList(5,5),tondeuse.getPosition());
+    }
+
+    public void testAvancerSurUnBordSuperieurHaut(){
+        tondeuse.setOrientation(Orientation.NORD);
         tondeuse.setCordX(5);
         tondeuse.setCordY(5);
         tondeuse.avancer(pelouse);
@@ -83,4 +174,6 @@ public class TondeuseTest extends TestCase {
         String absolutePath = file.getAbsolutePath();
         List<String> ligne = fichier.OuvrirFichier(absolutePath);
     }
+
+    
 }
